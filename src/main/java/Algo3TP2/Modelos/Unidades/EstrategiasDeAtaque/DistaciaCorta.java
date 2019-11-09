d9825ed239e0ffd4c8a870e52295029b741071cb
Nazareno <nazarodriguez56@gmail.com>
2019-11-08 21:29:37 -0300

    Ahora el tablero contiene loss metodos para medir la distancia entre dos casillas, de modo que lo pude quitar del curandero y de estrategia de ataque

--- src/main/java/Algo3TP2/Modelos/Unidades/EstrategiasDeAtaque/DistaciaCorta.java
+++ src/main/java/Algo3TP2/Modelos/Unidades/EstrategiasDeAtaque/DistaciaCorta.java
@@ -1,5 +1,7 @@
 package Algo3TP2.Modelos.Unidades.EstrategiasDeAtaque;
 
+import Algo3TP2.Modelos.Casillero.Casillero;
+import Algo3TP2.Modelos.Tablero.Tablero;
 import Algo3TP2.Modelos.Unidades.EstrategiasDeAtaque.ExcepcionesAtaque.DistanciaDeAtaqueIncorrectaExcepcion;
 import Algo3TP2.Modelos.Unidades.EstrategiasDeAtaque.ExcepcionesAtaque.UnidadAtacadaEsAliadaExcepcion;
 import Algo3TP2.Modelos.Unidades.Unidad;
@@ -9,11 +11,17 @@ public abstract class DistaciaCorta extends EstrategiaDeAtaque {
 
     public void validarAtaque(Unidad unidadAtacante, Unidad unidadVictima)
             throws DistanciaDeAtaqueIncorrectaExcepcion, UnidadAtacadaEsAliadaExcepcion {
+
         this.unidadVictimaEsEnemigaDeUnidadAtacante(unidadAtacante, unidadVictima);
-        int distanciaEnX = this.medirDistanciaEnX(unidadAtacante, unidadVictima);
-        int distanciaEnY = this.medirDistanciaEnY(unidadAtacante, unidadVictima);
+
+        Casillero casilleroAtacante = unidadAtacante.getCasillero();
+        Casillero casilleroVictima = unidadVictima.getCasillero();
+        int distanciaEnX = Tablero.getTablero().medirDistanciaEnXDeCasilleroACasillero(casilleroAtacante,casilleroVictima);
+        int distanciaEnY = Tablero.getTablero().medirDistanciaEnYDeCasilleroACasillero(casilleroAtacante,casilleroVictima);
+
         boolean condicionDistancia = (distanciaEnX <= Properties.alcanceMaxDistanciaCorta &&
                 distanciaEnY <= Properties.alcanceMaxDistanciaCorta);
+
         if (!condicionDistancia) {
             throw new DistanciaDeAtaqueIncorrectaExcepcion();
         }
