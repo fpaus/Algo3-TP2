7a658405a0662c1d2b9cf1a9837393e7f8d8792f
Francisco Yanes <franciscoyanesk@gmail.com>
2019-11-09 19:49:08 -0300

    Creación de las firmas de los tests de la Entrega 2

---
+++ src/test/java/Algo3TP2/CatedraTest/Entrega2Test/JineteTest.java
@@ -0,0 +1,23 @@
+package Algo3TP2.CatedraTest.Entrega2Test;
+
+import org.junit.Test;
+
+public class JineteTest {
+
+    @Test
+    /*Un jinete sin aliados en distancia corta y un enemigo
+    en distancia corta, ataca con su espada al enemigo y se
+    verifica que se realiza correctamente el ataque.*/
+    public void jineteSinAliadosADistanciaCortaAtacaConEspada(){
+
+    }
+
+    @Test
+    /*Un jinete sin aliados en distancia corta y un enemigo
+    en distancia corta y otro enemigo en distancia media,
+    trata de atacar al enemigo en distancia media y se
+    verifica que no se puede realizar el ataque.*/
+    public void jineteSinAliadosADistanciaMediaNoPuedeAtacar(){
+
+    }
+}
