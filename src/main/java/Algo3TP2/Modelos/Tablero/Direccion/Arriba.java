6cf2e69ef06ce6971868168848ab8bc89a5f220e
Francisco Yanes <franciscoyanesk@gmail.com>
2019-11-18 19:17:51 -0300

    Direcciones

--- src/main/java/Algo3TP2/Modelos/Tablero/Direccion/Arriba.java
+++ src/main/java/Algo3TP2/Modelos/Tablero/Direccion/Arriba.java
@@ -1,4 +1,10 @@
 package Algo3TP2.Modelos.Tablero.Direccion;
 
-public class Arriba {
+public class Arriba extends Vertical {
+
+
+    @Override
+    public int getDireccion() {
+        return 1;
+    }
 }
