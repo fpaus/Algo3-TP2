3bfe7a92373f047f9de22a0b5c4e9879323a5c49
Nazareno <nazarodriguez56@gmail.com>
2019-11-21 14:40:12 -0300

    Fix getCasilleroVecino(vertical, horizontal) ahora es (horizontal, vertical) se corresponde mas a la idea (x,y)

--- src/main/java/Algo3TP2/Modelos/Unidades/Movimientos.java
+++ src/main/java/Algo3TP2/Modelos/Unidades/Movimientos.java
@@ -16,41 +16,41 @@ public class Movimientos {
 
     public void ejecutarMovimientoHaciaLaIzquierda(UnidadMovible unidad, Casillero casilleroActual)
             throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Fija(), new Izquierda()));
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Izquierda(), new Fija()));
     }
 
     public void ejecutarMovimientoHaciaLaDerecha(UnidadMovible unidad, Casillero casilleroActual)
             throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Fija(), new Derecha()));
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Derecha(), new Fija()));
     }
 
     public void ejecutarMovimientoHaciaArriba(UnidadMovible unidad, Casillero casilleroActual)
-        throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Arriba(), new Fija()));
+            throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Fija(), new Arriba()));
     }
 
     public void ejecutarMovimientoHaciaAbajo(UnidadMovible unidad, Casillero casilleroActual)
-        throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Abajo(), new Fija()));
+            throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Fija(), new Abajo()));
     }
 
     public void ejecutarMovimientoHaciaLaIzquierdaAbajo(UnidadMovible unidad, Casillero casilleroActual)
             throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Abajo(), new Izquierda()));
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Izquierda(), new Abajo()));
     }
 
     public void ejecutarMovimientoHaciaLaIzquierdaArriba(UnidadMovible unidad, Casillero casilleroActual)
             throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Arriba(), new Izquierda()));
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Izquierda(), new Arriba()));
     }
 
     public void ejecutarMovimientoHaciaLaDerechaAbajo(UnidadMovible unidad, Casillero casilleroActual)
             throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Abajo(), new Derecha()));
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Derecha(), new Abajo()));
     }
 
     public void ejecutarMovimientoHaciaLaDerechaArriba(UnidadMovible unidad, Casillero casilleroActual)
             throws CasilleroFueraDelLosLimitesDelTableroExcepcion, MovimientoNoContiguoExcepcion, CasilleroOcupadoExcepcion, CasilleroVacioExcepcion {
-        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Arriba(), new Derecha()));
+        this.ejecutarMovimiento(unidad, casilleroActual.getCasilleroVecino(new Derecha(), new Arriba()));
     }
 }
