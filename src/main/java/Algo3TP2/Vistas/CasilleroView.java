3a3f66500019c3ee4b8e7378628c45bca2611b48
Francisco Yanes <franciscoyanesk@gmail.com>
2019-11-28 17:47:56 -0300

    Agregado clickeo de Casilleros

--- src/main/java/Algo3TP2/Vistas/CasilleroView.java
+++ src/main/java/Algo3TP2/Vistas/CasilleroView.java
@@ -6,7 +6,10 @@ import Algo3TP2.Modelos.Casillero.Casillero;
 import Algo3TP2.Modelos.Tablero.Coordenada;
 import Algo3TP2.Modelos.Tablero.Tablero;
 import Algo3TP2.Modelos.Tablero.ExcepcionesTablero.CasilleroFueraDelLosLimitesDelTableroExcepcion;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+
 
 public class CasilleroView extends Pane {
 
@@ -21,6 +24,15 @@ public class CasilleroView extends Pane {
         this.casillero = tablero.getCasilleroEnCoordenada(coordenada);
         this.setMinWidth(this.anchoDelCasillero);
         this.setMinHeight(this.largoDelCasillero);
+
+        final int[] num = {1}; // Para contar los clicks dentro de cada Pane.
+        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Casillero clickeado " + (Integer.toString(num[0])) + " veces...");
+                num[0]++;
+            }
+        });
 
         //this.setStyle("-fx-border-width: 5; -fx-border-width: 1; -fx-border-color: black;"); // Esta línea no sirve ya que Azul y Rojo setean los styles.
     }
