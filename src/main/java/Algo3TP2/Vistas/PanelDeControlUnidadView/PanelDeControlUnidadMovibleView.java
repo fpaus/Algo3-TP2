114c9b794ca5237c3e89c877f555cc981519628c
Nazareno <nazarodriguez56@gmail.com>
2019-12-01 15:01:06 -0300

    Ahora los botonera de movimiento es una clase

--- src/main/java/Algo3TP2/Vistas/PanelDeControlUnidadView/PanelDeControlUnidadMovibleView.java
+++ src/main/java/Algo3TP2/Vistas/PanelDeControlUnidadView/PanelDeControlUnidadMovibleView.java
@@ -13,25 +13,8 @@ public class PanelDeControlUnidadMovibleView extends PanelDeControlView {
         this.setPadding(new Insets(40, 40, 40, 40));
         this.setGridLinesVisible(true);
 
-        // /* Botonera Movimiento
-        Button botonMoverHaciaDerecha = new BotonMoverHaciaDerecha(unidad);
-        Button botonMoverHaciaIzquierda = new BotonMoverHaciaIzquierda(unidad);
-        Button botonMoverHaciaArriba = new BotonMoverHaciaArriba(unidad);
-        Button botonMoverHaciaAbajo = new BotonMoverHaciaAbajo(unidad);
-        Button botonMoverHaciaArribaIzquierda = new ButonMoverHaciaArribaIzquierda(unidad);
-        Button botonMoverHaciaArribaDerecha = new BotonMoverHaciaArribaDerecha(unidad);
-        Button botonMoverHaciaAbajoIzquierda = new BotonMoverHaciaAbajoIzquierda(unidad);
-        Button botonMoverHaciaAbajoDerecha = new BotonMoverHaciaAbajoDerecha(unidad);
-
-        this.add(botonMoverHaciaArribaIzquierda,0,0);
-        this.add(botonMoverHaciaArriba, 1,0);
-        this.add(botonMoverHaciaArribaDerecha,2,0);
-        this.add(botonMoverHaciaDerecha,2,1);
-        this.add(botonMoverHaciaIzquierda, 0,1);
-        this.add(botonMoverHaciaAbajoIzquierda,0,2);
-        this.add(botonMoverHaciaAbajo,1,2);
-        this.add(botonMoverHaciaAbajoDerecha,2,2);
-        //          Botonera Movimiento */
+        BotoneraMovimiento botoneraMovimiento = new BotoneraMovimiento(unidad);
+        this.add(botoneraMovimiento,1,1);
 
     }
 }
