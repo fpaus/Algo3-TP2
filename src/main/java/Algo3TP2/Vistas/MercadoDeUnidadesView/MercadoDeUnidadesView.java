d4445502266a17da93f641814cd6f381ff38e244
Nazareno <nazarodriguez56@gmail.com>
2019-12-04 20:39:41 -0300

    El oro disponible se actualiza a traves del observer

--- src/main/java/Algo3TP2/Vistas/MercadoDeUnidadesView/MercadoDeUnidadesView.java
+++ src/main/java/Algo3TP2/Vistas/MercadoDeUnidadesView/MercadoDeUnidadesView.java
@@ -4,6 +4,8 @@ import Algo3TP2.Controladores.ComprarUnidadHandler;
 import Algo3TP2.Modelos.Bando;
 import Algo3TP2.Modelos.Juego;
 import Algo3TP2.Modelos.Tablero.ExcepcionesTablero.CasilleroFueraDelLosLimitesDelTableroExcepcion;
+import Algo3TP2.ObservadorPuntos;
+import Algo3TP2.Properties;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -12,7 +14,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
-public class MercadoDeUnidadesView extends VBox {
+public class MercadoDeUnidadesView extends VBox implements ObservadorPuntos {
+
+    private Label labelOroRestante;
 
     public MercadoDeUnidadesView(Bando bando, Juego juego) throws CasilleroFueraDelLosLimitesDelTableroExcepcion {
 
@@ -29,9 +33,9 @@ public class MercadoDeUnidadesView extends VBox {
 
         ImageView imageViewMercadoTitulo = new ImageView(new Image("file:src/resources/Mercado/titulo_mercado.png"));
 
-
+        bando.getDuenio().enlazarObservador(this);
         StackPane stackPaneBilletera = new StackPane();
-        Label labelOroRestante = new Label("20");
+        this.labelOroRestante = new Label(Integer.toString(Properties.puntosInicialesDisponibles));
         labelOroRestante.setStyle(" -fx-font-size:20px; " +
                 "                   -fx-text-fill: #ffcc39;" +
                 "                   -fx-font-weight: bold;");
@@ -39,6 +43,11 @@ public class MercadoDeUnidadesView extends VBox {
         stackPaneBilletera.getChildren().add(labelOroRestante);
 
         this.getChildren().addAll(imageViewMercadoTitulo, stackPaneBilletera);
+    }
+
+    @Override
+    public void actualizar(int puntos) {
+        this.labelOroRestante.setText(Integer.toString(puntos));
     }
 }
 
