0ec4c5857644b10ef9de3de8e2f9cb917a0e47e9
Nazareno <nazarodriguez56@gmail.com>
2019-12-04 13:38:57 -0300

    Modificado el estilo de los botones de comprar unidad

--- src/main/java/Algo3TP2/Vistas/MercadoDeUnidadesView/MercadoDeUnidadesRojasView.java
+++ src/main/java/Algo3TP2/Vistas/MercadoDeUnidadesView/MercadoDeUnidadesRojasView.java
@@ -4,6 +4,7 @@ import Algo3TP2.Controladores.ComprarUnidadHandler;
 import Algo3TP2.Modelos.Bando;
 import Algo3TP2.Modelos.Juego;
 import Algo3TP2.Modelos.Tablero.ExcepcionesTablero.CasilleroFueraDelLosLimitesDelTableroExcepcion;
+import Algo3TP2.Vistas.BotonesPanelDeControl.BotonComprarUnidad;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -16,21 +17,22 @@ public class MercadoDeUnidadesRojasView extends MercadoDeUnidadesView {
         super(bando, juego);
 
         // Botones placeholders
-        Button botonUnidad1 = new Button();
-        botonUnidad1.setGraphic(new ImageView(new Image("file:src/resources/Mercado/boton_comprar_soldado_rojo.png")));
-        botonUnidad1.setOnAction(new ComprarUnidadHandler(colorEquipo, "Soldado", bando, juego));
+        Image botonUnidad1Image = new Image("file:src/resources/Mercado/boton_comprar_soldado_rojo.png");
+        ComprarUnidadHandler botonUnidad1Handler = new ComprarUnidadHandler(colorEquipo, "Soldado", bando, juego);
+        Button botonUnidad1 = new BotonComprarUnidad(botonUnidad1Handler, botonUnidad1Image);
 
-        Button botonUnidad2 = new Button();
-        botonUnidad2.setGraphic(new ImageView(new Image("file:src/resources/Mercado/boton_comprar_jinete_rojo.png")));
-        botonUnidad2.setOnAction(new ComprarUnidadHandler(colorEquipo, "Jinete", bando, juego));
+        Image botonUnidad2Image = new Image("file:src/resources/Mercado/boton_comprar_jinete_rojo.png");
+        ComprarUnidadHandler botonUnidad2Handler = new ComprarUnidadHandler(colorEquipo, "Jinete", bando, juego);
+        Button botonUnidad2 = new BotonComprarUnidad(botonUnidad2Handler, botonUnidad2Image);
 
-        Button botonUnidad3 = new Button();
-        botonUnidad3.setGraphic(new ImageView(new Image("file:src/resources/Mercado/boton_comprar_curandero_rojo.png")));
-        botonUnidad3.setOnAction(new ComprarUnidadHandler(colorEquipo, "Curandero", bando, juego));
+        Image botonUnidad3Image = new Image("file:src/resources/Mercado/boton_comprar_curandero_rojo.png");
+        ComprarUnidadHandler botonUnidad3Handler = new ComprarUnidadHandler(colorEquipo, "Curandero", bando, juego);
+        Button botonUnidad3 = new BotonComprarUnidad(botonUnidad3Handler, botonUnidad3Image);
 
-        Button botonUnidad4 = new Button();
-        botonUnidad4.setGraphic(new ImageView(new Image("file:src/resources/Mercado/boton_comprar_catapulta_roja.png")));
-        botonUnidad4.setOnAction(new ComprarUnidadHandler(colorEquipo, "Catapulta", bando, juego));
+        Image botonUnidad4Image = new Image("file:src/resources/Mercado/boton_comprar_catapulta_roja.png");
+        ComprarUnidadHandler botonUnidad4Handler = new ComprarUnidadHandler(colorEquipo, "Catapulta", bando, juego);
+        Button botonUnidad4 = new BotonComprarUnidad(botonUnidad4Handler, botonUnidad4Image);
+
 
         this.getChildren().addAll(botonUnidad1, botonUnidad2, botonUnidad3, botonUnidad4);
     }
